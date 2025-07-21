672f88f5f9498136934e3db4db2236a444e30f9b
afrozkhan19 <afroz.19.khan@gmail.com>
2025-07-21 16:04:37 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,13 @@
+
+
+    public class Hello
+    {
+       public static void main(String args[])
+        {
+             int aa=60;
+System.out.println("the variable value is"+aa);
+
+	}
+
+   }
+      
